4332d876e36d9ea94969b1f226ba094096a30201
GENI77 <pavlenko_genj@mail.ru>
2024-07-27 14:35:39 +1000

    Add files via upload

--- module3hard.py
+++ module3hard.py
@@ -4,7 +4,7 @@ data = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",
         ((), [{(2, 'Urban', ('Urban2', 35))}])]
 
 
-def calculate_structure_sum(*args):
+def calculate_structure_sum(data):
     summa = 0
     if isinstance(data, str):
         return len(data)
@@ -17,7 +17,7 @@ def calculate_structure_sum(*args):
         for k, v in data.items():
             summa += calculate_structure_sum(k)
             summa += calculate_structure_sum(v)
-        return summa
+    return summa
 
 
 result = calculate_structure_sum(data)
