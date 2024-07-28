f9afa510b3519ea83fecd7b79453e0cc0a90f875
GENI77 <pavlenko_genj@mail.ru>
2024-07-28 15:06:12 +1000

    Add files via upload

---
+++ feke_math.py
@@ -0,0 +1,13 @@
+from math import inf
+def divide(first, second):
+    if second == 0:
+        result = inf
+    else:
+        result = first / second
+    return result
+
+
+# result1 = divide(30, 10)
+# result2 = divide(15, 0)
+# print(result1)
+# print(result2)
