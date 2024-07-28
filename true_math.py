284f9c4852f182aeb562de3ebbdb13f038c517a0
GENI77 <pavlenko_genj@mail.ru>
2024-07-28 15:05:53 +1000

    Add files via upload

---
+++ true_math.py
@@ -0,0 +1,12 @@
+def divide(first, second):
+    if second == 0:
+        result = 'Ошибка'
+    else:
+        result = first / second
+    return result
+
+
+# result1 = divide(50, 5)
+# result2 = divide(13, 0)
+# print(result1)
+# print(result2)
