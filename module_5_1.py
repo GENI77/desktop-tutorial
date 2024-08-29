7672de096cf7936dc5eebf5c896c85591255eff0
GENI77 <pavlenko_genj@mail.ru>
2024-08-29 14:24:10 +1000

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,27 @@
+# Создайте класс House.
+# Вунтри класса House определите метод __init__, в который передадите название и кол-во этажей.
+# Внутри метода __init__ создайте атрибуты объекта self.name и self.number_of_floors, присвойте им переданные значения.
+# Создайте метод go_to с параметром new_floor и напишите логику внутри него на основе описания задачи.
+# Создайте объект класса House с произвольным названием и количеством этажей.
+# Вызовите метод go_to у этого объекта с произвольным числом.
+class House:
+    def __init__(self, name, number):
+        self.name = name
+        self.number_of_floors = number
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print('Такого этажа не существует')
+        else:
+            for new_floor in range(1, new_floor + 1):
+                print(f'этаж {new_floor}')
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to = int(5)
+h2.go_to = int(10)
+print(h1.name, h1.number_of_floors)
+print(h2.name, h2.number_of_floors)
+print(h1.go_to)
+print(h2.go_to)
